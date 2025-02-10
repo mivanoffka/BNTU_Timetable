100dab021bdd69891133bea916422bb6f865ff72
mivanoffka <sharariki@gmail.com>
2025-02-10 20:39:49 +0300

    Обновлённые подписи к ссылкам на фитре

--- parsing/autoparser.py
+++ parsing/autoparser.py
@@ -16,6 +16,7 @@ import re
 
 file_index = 0
 
+
 def get_html(url):
     counter = 0
     f = None
@@ -70,7 +71,7 @@ def download(url, dest):
 
 
 def find_lines_with_url_ef():
-    ref = 'https://bntu.by/faculties/ef/pages/raspisanie-zanyatij-dnevnoe-otdelenie'
+    ref = "https://bntu.by/faculties/ef/pages/raspisanie-zanyatij-dnevnoe-otdelenie"
 
     html = requests.get(ref, verify=False)
     html = html.text
@@ -85,33 +86,42 @@ def find_lines_with_url_ef():
             if key in line:
                 urls.append(line)
                 break
-    
+
     new_urls = []
 
     new_url_1 = urls[0]
-    new_url_1 = new_url_1.replace('<td style="width: 26.343%; height: 22px; text-align: center;"><strong><a href="', "",)
+    new_url_1 = new_url_1.replace(
+        '<td style="width: 26.343%; height: 22px; text-align: center;"><strong><a href="',
+        "",
+    )
     new_url_1 = new_url_1.replace('">Скачать</a></strong></td>', "")
-    new_urls.append(new_url_1) 
+    new_urls.append(new_url_1)
 
     new_url_2 = urls[1]
-    new_url_2 = new_url_2.replace('<td style="width: 26.343%; height: 22px; text-align: center;"><strong><a href="', "")
+    new_url_2 = new_url_2.replace(
+        '<td style="width: 26.343%; height: 22px; text-align: center;"><strong><a href="',
+        "",
+    )
     new_url_2 = new_url_2.replace('" title="Скачать">Скачать</a></strong></td>', "")
-    new_urls.append(new_url_2) 
+    new_urls.append(new_url_2)
 
     return new_urls
 
+
 def find_lines_with_urld_fitr():
-    ref = 'https://bntu.by/faculties/fitr/pages/raspisanie-zanyatij-i-ekzamenov'
+    ref = "https://bntu.by/faculties/fitr/pages/raspisanie-zanyatij-i-ekzamenov"
 
     html = requests.get(ref, verify=False)
     html = html.text
 
     html = html.splitlines()
 
-    keys = ('Расписание учебных занятий студентов 3-го курса специальности <strong>1-40 01 01</strong> дневной формы получения образования с 27 января по 17 мая 2024-2025 учебного года</span></a></p>',
-            'Расписание учебных занятий студентов 3-го курса специальности <strong>1-40 05 01</strong> дневной формы получения образования с 27 января по 17 мая 2024-2025 учебного года</span></a></p>',
-            'Расписание учебных занятий студентов 3 и 4-го курсов специальности <strong>1-53 01 01 и 1-53 01 06</strong> дневной формы получения образования&nbsp;</span></a></p>',
-            'Расписание учебных занятий студентов 3 и 4-го курсов специальности <strong>1-53 01 05</strong> дневной формы получения образования с 10 февраля по 7 июня 2024-2025 учебного года</a></span></p>')
+    keys = (
+        "Расписание учебных занятий студентов 3-го курса специальности <strong>1-40 01 01</strong> дневной формы получения образования с 27 января по 17 мая 2024-2025 учебного года</span></a></p>",
+        "Расписание учебных занятий студентов 3-го курса специальности <strong>1-40 05 01</strong> дневной формы получения образования с 27 января по 17 мая 2024-2025 учебного года</span></a></p>",
+        "Расписание учебных занятий студентов 3 и 4-го курсов специальности <strong>1-53 01 01 и 1-53 01 06</strong> дневной формы получения образования&nbsp;</span></a></p>",
+        "Расписание учебных занятий студентов 3 и 4-го курсов специальности <strong>1-53 01 05</strong> дневной формы получения образования с 10 февраля по 7 июня 2024-2025 учебного года</span></a></p>",
+    )
     urls = []
 
     for key in keys:
@@ -134,8 +144,8 @@ def find_lines_with_urls():
     ref = "https://bntu.by/raspisanie"
     html = requests.get(ref, verify=False)
 
-    #html = get_html(ref)
-    #html = html.decode("utf-8")
+    # html = get_html(ref)
+    # html = html.decode("utf-8")
     html = html.text
     html = html.splitlines()
 
@@ -155,10 +165,8 @@ def find_lines_with_urls():
             ref_2 = line
             break
 
-
     result.append(ref_1)
     result.append(ref_2)
-
 
     return result
 
@@ -189,18 +197,22 @@ def get_url_from_line_fitr(stroke: str):
 
 
 def get_link_for_cource(course_number):
-    res = requests.get(f'https://schedule.bntu.by:9443/api/schedule_files/?course={course_number}&format=json', verify=False)
+    res = requests.get(
+        f"https://schedule.bntu.by:9443/api/schedule_files/?course={course_number}&format=json",
+        verify=False,
+    )
 
     if not res.text:
         return
 
     res_json = res.json()
 
-    excel_links = [item['excel_files'] for item in res_json]
+    excel_links = [item["excel_files"] for item in res_json]
 
     logging.info(excel_links)
 
     return excel_links[file_index][0]
+
 
 def clear_folder():
     folder_path = Path(BASE_DIR / "parsing/sheets")
@@ -214,6 +226,7 @@ def clear_folder():
                 os.remove(file_path)
         except:
             ...
+
 
 def download_and_parse():
     clear_folder()
@@ -252,17 +265,25 @@ def download_and_parse():
         destination = Path(BASE_DIR / "parsing/sheets/34kurs_fitr_4.xls")
         download_unsafe(get_url_from_line_fitr(urls[3]), destination)
 
-        download_unsafe("https://drive.google.com/uc?export=download&id=1UwvtDysgPIVdNyNIDi-f_zkqASJrnZLz",
-                        Path(BASE_DIR/"parsing/sheets/3kurs_fmmp_1.xls"))
+        download_unsafe(
+            "https://drive.google.com/uc?export=download&id=1UwvtDysgPIVdNyNIDi-f_zkqASJrnZLz",
+            Path(BASE_DIR / "parsing/sheets/3kurs_fmmp_1.xls"),
+        )
 
-        download_unsafe("https://drive.google.com/uc?export=download&id=1RNrL10SO_0EOpOSPgipw2iqx3iCidcPC",
-                        Path(BASE_DIR/"parsing/sheets/3kurs_fmmp_2.xls"))
+        download_unsafe(
+            "https://drive.google.com/uc?export=download&id=1RNrL10SO_0EOpOSPgipw2iqx3iCidcPC",
+            Path(BASE_DIR / "parsing/sheets/3kurs_fmmp_2.xls"),
+        )
 
-        download_unsafe("https://drive.google.com/uc?export=download&id=1OZblTd85WnWxi6KYHk9kx3aE4A-R5F2W",
-                        Path(BASE_DIR/"parsing/sheets/3kurs_fmmp_3.xls"))
+        download_unsafe(
+            "https://drive.google.com/uc?export=download&id=1OZblTd85WnWxi6KYHk9kx3aE4A-R5F2W",
+            Path(BASE_DIR / "parsing/sheets/3kurs_fmmp_3.xls"),
+        )
 
-        download_unsafe("https://drive.google.com/uc?export=download&id=1k3Ff4AZJP6PL3TH3ilEHrbav369n4WTj",
-                        Path(BASE_DIR/"parsing/sheets/4kurs_fmmp_1.xls"))
+        download_unsafe(
+            "https://drive.google.com/uc?export=download&id=1k3Ff4AZJP6PL3TH3ilEHrbav369n4WTj",
+            Path(BASE_DIR / "parsing/sheets/4kurs_fmmp_1.xls"),
+        )
 
         logging.info("Books are downloaded.")
         download_result = True
@@ -278,7 +299,6 @@ def download_and_parse():
         except Exception as e:
             logging.info(f"An exception occured while parsing the sheets. \n\n{e}")
             raise
-
 
 
 if __name__ == "__main__":
